97b87032acdebb4573a6dd6f78c751b35ee9823c
Saurabh <saurabh.verma2892@gmail.com>
2018-03-20 14:36:27 -0600

    all good

--- services/apiService.js
+++ services/apiService.js
@@ -5,6 +5,7 @@ module.exports = app => {
     let User = app.models.user;
     
     function loginUser(params){
+        console.log("apiService:loginUser");
         return User.loginUser(params);
     }
 
